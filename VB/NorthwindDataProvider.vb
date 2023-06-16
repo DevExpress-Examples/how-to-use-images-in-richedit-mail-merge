6b808cb035e0768122e99cc185ce0650fb5a0558
DevExpressExampleBot <fakeEmail>
2023-06-16 15:01:12 +0400

    Source auto update [skip ci]

--- VB/NorthwindDataProvider.vb
+++ VB/NorthwindDataProvider.vb
@@ -1,34 +1,29 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Data
 Imports System.Data.SqlClient
 
 Namespace RichEditImageMailMerge
-	Public NotInheritable Class NorthwindDataProvider
-		Private Shared connectionString As String = "Data Source=.\SQLExpress;Initial Catalog=Northwind;Integrated Security=SSPI"
 
-		Private Sub New()
-		End Sub
-		Shared Sub New()
-			Using connection As New SqlConnection(connectionString)
-				Dim selectCommand As New SqlCommand("SELECT * FROM Categories", connection)
-				Dim da As New SqlDataAdapter(selectCommand)
+    Public Module NorthwindDataProvider
 
-				categories_Renamed = New DataTable("Categories")
+        Private connectionString As String = "Data Source=.\SQLExpress;Initial Catalog=Northwind;Integrated Security=SSPI"
 
-				da.Fill(categories_Renamed)
+        Sub New()
+            Using connection As SqlConnection = New SqlConnection(connectionString)
+                Dim selectCommand As SqlCommand = New SqlCommand("SELECT * FROM Categories", connection)
+                Dim da As SqlDataAdapter = New SqlDataAdapter(selectCommand)
+                categoriesField = New DataTable("Categories")
+                da.Fill(categoriesField)
+                categoriesField.Constraints.Add("IDPK", categoriesField.Columns("CategoryID"), True)
+                selectCommand.Dispose()
+            End Using
+        End Sub
 
-				categories_Renamed.Constraints.Add("IDPK", categories_Renamed.Columns("CategoryID"), True)
+        Private categoriesField As DataTable
 
-				selectCommand.Dispose()
-			End Using
-		End Sub
-
-		Private Shared categories_Renamed As DataTable
-
-		Public Shared ReadOnly Property Categories() As DataTable
-			Get
-				Return categories_Renamed
-			End Get
-		End Property
-	End Class
+        Public ReadOnly Property Categories As DataTable
+            Get
+                Return categoriesField
+            End Get
+        End Property
+    End Module
 End Namespace
